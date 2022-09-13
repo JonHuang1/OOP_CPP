dc24857d1ee4b99275b0988efd0023630f9c28f8
Jon Huang <jonhuang918@gmail.com>
2022-09-13 09:07:34 -0400

    Finished

---
+++ HW3/userid/DataBin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "DataBin.h"
+
+DataBin::DataBin(int _datum) {
+   datum = new int(_datum);
+}
+
+int DataBin::getDatum( ) {
+   return *datum;
+}
+
+DataBin::~DataBin( ) {
+   std::cout << "deleting DataBin " << *datum << std::endl;
+}
