6db555ee58a7f3c64f9f4633c616bd442528e4d4
JonHuang1 <jonhuang918@gmail.com>
2022-12-05 22:28:50 -0500

    HW13

---
+++ HW13/huan1588/vehicle/FordF350.java
@@ -0,0 +1,37 @@
+package huan1588.vehicle;
+// import huan1588.vehicle.Vehicle;
+
+public class FordF350 extends Vehicle {
+
+   String color;
+
+   public FordF350(String _color) {
+      super("Ford F350");
+      color = _color;
+      System.out.println(" built");
+   }
+
+   protected String getModel( ) { 
+      return model;
+   }
+
+   public String getColor( ) { 
+      return color;
+   }
+
+   public String getEngine( ) {
+      return "7.3L V8";
+   }
+
+   public String getTransmission( ) {
+      return "10 speed automatic";
+   }
+
+   public int getNumPassengers( ) {
+      return 3;
+   }
+
+   public String getWarranty( ) {
+      return "3/36,000 + 5/60,000 Powertrain";
+   } 
+}
